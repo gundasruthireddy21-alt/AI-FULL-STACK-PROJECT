4b8333bb08488da74f0376a269820e374b24c43a
Gunda Sruthi <gundasruthireddy@21gmail.com>
2026-09-26 11:48:24 +0530

    Day05

--- Day05/simple_App.py
+++ Day05/simple_App.py
@@ -2,5 +2,6 @@ import streamlit as st
 st.title("My first streamlit App!!!")
 st.write("welcome to my AI application!")
 name = st.text_input("Enter your name: ")
-if st.button("submit"):
-    st.write("Hello", name)
+st.write("Hello", name)
+
+
